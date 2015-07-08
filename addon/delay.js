232d0a0151fb8f3e8fbd9b683411e92049810ad5
Gavin Joyce <gavinjoyce@gmail.com>
2015-07-08 16:52:00 +0100

    fix bug where delay 0 would result in delay 2000

--- addon/delay.js
+++ addon/delay.js
@@ -1,7 +1,10 @@
 import Em from 'ember';
 
 var delay = function(milliseconds) {
-  milliseconds = milliseconds || 2000;
+  if(milliseconds === undefined) {
+    milliseconds = 2000;
+  }
+
   return new Em.RSVP.Promise(function(resolve) {
     Em.run.later(this, resolve, milliseconds);
   });
